b50c3f58e8dccde14558a395d39c54ddaf2e9c59
Yevgeni <yevgenikuz@gmail.com>
2020-12-19 16:56:15 +0200

    .

--- WindowsHandler.py
+++ WindowsHandler.py
@@ -3,25 +3,30 @@ from tkinter import messagebox
 import tkinter as tk
 
 
+#####################################################################################################################
+# Singleton Realization:                                                                                            #
+# Once the player has selected the type of game he wants to play it will not be possible to open a new game window. #
+# Only after the player close the game window it will be possible to select another game type or the same type game.#
+#####################################################################################################################
 class WindowsHandler:
-    // The number of instances of the object at a given moment
+    # The number of instances of the object at a given moment
     _number_of_times = 0
 
     def __init__(self):
         self.moves = []
         self.gameWindow = None
 
-    // close currently window
+    # close currently window
     def close_window(self):
-        // _number_of_times Decreases by 1
+        #_number_of_times Decreases by 1
         WindowsHandler._number_of_times -= 1
         self.gameWindow.destroy()
 
     def openNewWindow(self, game_mode):
-        // Check if there is already an instance of the object - If so you can not create another one
+        # Check if there is already an instance of the object - If so you can not create another one
         if WindowsHandler._number_of_times >= 1:
             messagebox.showerror("Tic Tac Toe", "You can't open more than one window")
-        // If no new instance of the object is created and _number_of_times Increases by 1
+        # If no new instance of the object is created and _number_of_times Increases by 1
         else:
             self.gameWindow = tk.Tk()
             WindowsHandler._number_of_times += 1
