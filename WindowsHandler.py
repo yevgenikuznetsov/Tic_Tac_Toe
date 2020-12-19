220263d425ee6e0b5072a4790aea453548548a74
Ofir <70342090+OfirCohen26@users.noreply.github.com>
2020-12-19 15:54:54 +0200

    Update WindowsHandler.py
    add Documentation

--- WindowsHandler.py
+++ WindowsHandler.py
@@ -4,19 +4,24 @@ import tkinter as tk
 
 
 class WindowsHandler:
+    // The number of instances of the object at a given moment
     _number_of_times = 0
 
     def __init__(self):
         self.moves = []
         self.gameWindow = None
 
+    // close currently window
     def close_window(self):
+        // _number_of_times Decreases by 1
         WindowsHandler._number_of_times -= 1
         self.gameWindow.destroy()
 
     def openNewWindow(self, game_mode):
+        // Check if there is already an instance of the object - If so you can not create another one
         if WindowsHandler._number_of_times >= 1:
             messagebox.showerror("Tic Tac Toe", "You can't open more than one window")
+        // If no new instance of the object is created and _number_of_times Increases by 1
         else:
             self.gameWindow = tk.Tk()
             WindowsHandler._number_of_times += 1
